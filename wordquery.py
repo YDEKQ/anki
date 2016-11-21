0f2e08448804699c4f8a30aaa269c0d91a3370d6
Liang Feng <Liang Feng>
2016-11-21 23:47:08 +0200

    unable to build the signal and slot connection

--- wordquery.py
+++ wordquery.py
@@ -1,27 +1,31 @@
 #-*- coding:utf-8 -*-
 
-# import the main window object (mw) from aqt
+
 # We're going to add a menu item below. First we want to create a function to
 # be called when the menu item is activated.
 import os
+# import the main window object (mw) from aqt
 import re
 import sys
 import urllib2
 import xml.etree.ElementTree
+from collections import defaultdict
 from StringIO import StringIO
 
 import aqt
-from aqt import mw
-from aqt.qt import *
 from anki.hooks import addHook, runHook, wrap
 from anki.importing import TextImporter
+from aqt import mw
 from aqt.addcards import AddCards
 from aqt.modelchooser import ModelChooser
+from aqt.qt import *
 from aqt.studydeck import StudyDeck
 from aqt.toolbar import Toolbar
 from aqt.utils import shortcut, showInfo
 # import trackback
 from mdict.mdict_query import IndexBuilder
+
+
 # from Queue import Queue
 
 
@@ -32,24 +36,9 @@ savepath = os.path.join(sys.path[0], 'config')
 serveraddr = default_server
 use_local, use_server = False, False
 
+query_over = pyqtSignal(dict)
 
-def _my_center_links(self):
-    '''
-    todo: use wrap
-    '''
-    links = [
-            ["decks", _("Decks"), _("Shortcut key: %s") % "D"],
-            ["add", _("Add"), _("Shortcut key: %s") % "A"],
-            ["browse", _("Browse"), _("Shortcut key: %s") % "B"],
-            ["query", _("Query"), _("Shortcut key: %s") % "Q"],
-    ]
-    self.link_handlers["query"] = _show_query_window  # self._queryLinkHandler
-    # showInfo("custorm links")
-    return self._linkHTML(links)
-Toolbar._centerLinks = _my_center_links
-
-# build set dialog and process the dict path
-
+aqt.main.AnkiQt.query_over = query_over
 
 def select_dict():
     global dictpath
@@ -109,19 +98,9 @@ def index_mdx():
     mw.progress.finish()
 
 
-# AnkiQt.setupAddons = wrap(AnkiQt.setupAddons, index_mdx, "after")
-
-
 def onCheckLocalStageChanged():
-    global use_local
-    use_local = mw.myCheckLocal.isChecked()
-    if use_local:
+    if mw.myCheckLocal.isChecked():
         select_dict()
-
-
-def onCheckServerStageChanged():
-    global use_server
-    use_server = mw.myCheckServer.isChecked()
 
 
 def set_options():
@@ -137,7 +116,6 @@ def set_options():
 
     mw.myCheckServer = check_server = QCheckBox("Use MDX Server")
     check_server.setChecked(use_server)
-    check_local.stateChanged.connect(onCheckServerStageChanged)
     mw.myEditServerAddr = server_edit = QLineEdit()
     server_edit.setText(serveraddr)
 
@@ -153,102 +131,22 @@ def set_options():
     widget.show()
 
 
-#######################################################
-
-# "Query" Link in main window
-
-
-def _query_link():
-    word = mw.myWordEdit.text().strip()
-    if word:
-        # showInfo("Query %s" % word)
-        mw.myWebView.load(
-            QUrl('http://dict.baidu.com/s?wd=%s&ptype=english' % word))
-
-
-def _show_query_window():
-    mw.myWidget = widget = QWidget()
-    layout = QGridLayout()
-    mw.myWordEdit = word_edit = QLineEdit()
-    ok_button = QPushButton("OK")
-    ok_button.clicked.connect(_query_link)
-    mw.myWebView = result_view = QWebView()
-    layout.addWidget(QLabel("Word to query: "))
-    layout.addWidget(word_edit)
-    layout.addWidget(ok_button)
-    layout.addWidget(result_view)
-
-    widget.setLayout(layout)
-    widget.show()
-#################################################################
-
-# custom add ui
-
 
 def my_setupButtons(self):
     bb = self.form.buttonBox
     ar = QDialogButtonBox.ActionRole
     self.queryButton = bb.addButton(_("Query"), ar)
-    self.queryButton.clicked.connect(self.query)
+    self.queryButton.clicked.connect(query)
     self.queryButton.setShortcut(QKeySequence("Ctrl+Q"))
     self.queryButton.setToolTip(shortcut(_("Query (shortcut: ctrl+q)")))
+    self.mw.query_over.connect(self.update_field)
 
-
-def query(self):
-    for field in self.editor.note.fields:
-        field = ''
-    self.query_youdao()
-    self.query_mdict()
-    self.editor.currentField = 0
-    self.editor.loadNote()
-
-
-def query_youdao(self):
-    self.editor.saveAddModeVars()
-    note = self.editor.note
-    word = note.fields[0]      # choose the first field as the word
-    result = urllib2.urlopen(
-        "http://dict.youdao.com/fsearch?client=deskdict&keyfrom=chrome.extension&pos=-1&doctype=xml&xmlVersion=3.2&dogVersion=1.0&vendor=unknown&appVer=3.1.17.4208&le=eng&q=%s" % word, timeout=5).read()
-    file = StringIO(result)
-    doc = xml.etree.ElementTree.parse(file)
-    # fetch symbols
-    symbol, uk_symbol, us_symbol = doc.findtext(".//phonetic-symbol"), doc.findtext(
-        ".//uk-phonetic-symbol"), doc.findtext(".//us-phonetic-symbol")
-    # showInfo(','.join([symbol, uk_symbol, us_symbol]))
-    try:
-        note.fields[1] = 'UK [%s] US [%s]' % (uk_symbol, us_symbol)
-        # fetch explanations
-        note.fields[3] = '<br>'.join([node.text for node in doc.findall(
-            ".//custom-translation/translation/content")])
-    except:
-        showInfo("Template Error, online!")
-
-
-def query_mdict(self):
-    self.editor.saveAddModeVars()
-    note = self.editor.note
-    word = note.fields[0]      # choose the first field as the word
-    result = None
-    if use_local:
-        try:
-            if not index_builder:
-                index_mdx()
-            result = index_builder.mdx_lookup(word)
-            if result:
-                update_field(result[0], note)
-        except AssertionError as e:
-            # no valid mdict file found.
-            pass
-    if use_server:
-        try:
-            req = urllib2.urlopen(
-                serveraddr + r'/' + word)
-            result2 = req.read()
-            if result2:
-                update_field(result2, note)
-        except:
-            # server error
-            pass
+    
+def query():
+    d = defaultdict(str)
+    d.update(query_youdao2(word))
+    d.update(query_mdict2(word))
+    mw.query_over.emit(d)
 
 
 def convert_media_path(html):
@@ -270,35 +168,29 @@ def convert_media_path(html):
     return unicode(html)
 
 
-def update_field(result_text, note):
+fields = {0: u'英语单词', 1: u'英美音标', 3: u'简要中文释义', 8: u'柯林斯中文解释', 9: u'柯林斯英文解释', 10: u'牛津高阶解释',
+         11: u'牛津英语解释', 12: u'麦克米伦解释', 13: u'朗文当代解释', 14: u'韦氏大学解释'}
+
+
+def update_field(self, data):
+    note = self.editor.note
+    for field in note.fields:
+        field = ''
     if len(note.fields) < 15:
         showInfo("Template Error, Mdx!")
         return
-    result_text = convert_media_path(result_text)
-    if 'href="_collinsEC.css"' in result_text:
-        note.fields[8] = result_text
-    elif 'href="_CollinsEN.css"' in result_text:
-        note.fields[9] = result_text
-    if 'href="_O8C.css"' in result_text:
-        note.fields[10] = result_text
-    elif 'href="_ODE.css"' in result_text:  # ok
-        note.fields[11] = result_text
-    elif 'href="_MacmillanEnEn.css"' in result_text:  # ok
-        note.fields[12] = result_text
-    elif 'href="_LDOCE6.css"' in result_text:  # ok
-        note.fields[13] = result_text
-    elif 'href="_MWU.css"' in result_text:  # ok
-        note.fields[14] = result_text
-    else:
-        pass
+    for k, v in fields:
+        note.fields[k] = data[v]
+    self.editor.currentField = 0
+    self.editor.loadNote()
 
 
 use_local, dictpath, use_server, serveraddr = read_parameters()
 # showInfo(dictpath)
-AddCards.query = query
-AddCards.query_youdao = query_youdao
-AddCards.query_mdict = query_mdict
+AddCards.update_field = update_field
 AddCards.setupButtons = wrap(AddCards.setupButtons, my_setupButtons, "before")
+
+
 # create a new menu item, "test"
 action = QAction("Word Query", mw)
 # set it to call testFunction when it's clicked
@@ -307,12 +199,6 @@ action.triggered.connect(set_options)
 mw.form.menuTools.addAction(action)
 
 
-from collections import defaultdict
-
-deck_name = u"test"
-note_type_name = u"MultiDicts"
-
-expsdict = defaultdict(str)
 
 
 def query_youdao2(word):
